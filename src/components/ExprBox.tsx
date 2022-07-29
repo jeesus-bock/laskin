8dddd416fb03b85c24a672c85cc891e61d03cbc4
Jussi <jussi.pesonen@adaptek.fi>
2022-07-29 20:45:16 +0300

    Small edit.

--- src/components/ExprBox.tsx
+++ src/components/ExprBox.tsx
@@ -1,5 +1,5 @@
 import { Box, Center } from '@hope-ui/solid';
-import { Component, createMemo } from 'solid-js';
+import { Component } from 'solid-js';
 
 // ExprBox-komponentti näyttää lausekkeen laatikossa.
 // Se ottaa propseinaan expr:n joka on itse lauseke merkkijonona ja
